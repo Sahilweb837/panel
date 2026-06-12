6ff964629b8b60456f90060f7716e0dcd6370041
Sahilweb837 <sahilsandhu39234@gmail.com>
2026-06-12 15:04:39 +0530

    new code

--- replace_confirms.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path resources/views -Recurse -Filter *.blade.php
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName -Raw
-    $newContent = [regex]::Replace($content, 'onsubmit="return confirm\(''(.+?)''\);?"', 'onsubmit="return confirmAction(event, ''$1'');"')
-    if ($content -ne $newContent) {
-        Set-Content -Path $file.FullName -Value $newContent -NoNewline
-        Write-Host "Updated $($file.FullName)"
-    }
-}
